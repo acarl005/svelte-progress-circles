9e36c33b5a355c5b70984721184498ab4e1b10c7
Andrew Carlson <2yinyang2@gmail.com>
2022-04-10 22:18:11 -0400

    style a tad bit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
   <ProgressCircle progress={25} text="1/4" />
 </div>
 <div class="circle-container">
-  <ProgressCircle progress={40} fgColor="green" bgColor="black" />
+  <ProgressCircle progress={40} fgColor="greenyellow" bgColor="black" />
 </div>
 <div class="circle-container">
   <ProgressCircle progress={50} />
@@ -31,6 +31,10 @@
 </div>
 
 <style>
+  :global(body) {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+
   .circle-container {
     width: 200px;
     height: 200px;
